4435c1315119af36c168defde6443daa1c548c6f
Ryan Taylor <ryan.taylor1@yahoo.com>
2022-10-25 12:10:19 -0700

    added timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,45 @@
-var datetime = null,
-  date = null;
 
-var update = function () {
-  date = moment(new Date());
-  datetime.html(date.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+function populatePage(){
+    for(let i=9; i<18;i++){
+        $('.container').append(`
+        <div class ="row">
+          <h1 class="time-block col-2">${i}:00</h1>
+          <textarea class="col-9 ${addClass(i)}"></textarea>
+          <button class="saveBtn col-1">Save</button>
+        </div> 
+        `)
+    }
+
+    addEventListenersToButtons()
+}
+
+function addEventListenersToButtons(){
+    console.log("adding listeners")
+    //get all the buttons
+
+    console.log(variable)
+    //have to iterate through them and add an event listener
+}
+function addClass(hour){
+
+    if(hour < moment().hour()){
+        return "past"
+    } 
+    if(hour>moment().hour()){
+        return "future"
+    }
+    return "present"
+    // see how I can get the hour of the day in military time
+}
+
+const update = function () {
+    let datetime = $('#currentDay');
+    let date = moment(new Date());
+    datetime.html(date.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 };
 
 $(document).ready(function(){
-    datetime = $('#currentDay')
     update();
-    setInterval(update, 1000);
+    setInterval(update, 1000);   
+    populatePage()
 });
