8e255b2a90ab87ab6461158f9c79d97773032784
Ryan Taylor <ryan.taylor1@yahoo.com>
2022-10-27 18:09:40 -0700

    added code to create event listeners for save buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
+const containerEl = $('.container');
 
 function populatePage(){
     for(let i=9; i<18;i++){
-        $('.container').append(`
+        containerEl.append(`
         <div class ="row">
           <h1 class="time-block col-2">${i}:00</h1>
-          <textarea class="col-9 ${addClass(i)}"></textarea>
+          <textarea class="col-9 ${addClass(i)} text-area"></textarea>
           <button class="saveBtn col-1">Save</button>
         </div> 
         `)
@@ -13,11 +14,34 @@ function populatePage(){
     addEventListenersToButtons()
 }
 
+
 function addEventListenersToButtons(){
     console.log("adding listeners")
     //get all the buttons
+    let saveButton = $('.saveBtn');
 
-    console.log(variable)
+    //console.log(textArea[4])
+    saveButton.on('click', function(event){
+        const text = $(event.target).closest(".row").find('.text-area').val();
+        const hour = $(event.target).closest(".row").find('.time-block').text();
+        console.log("call");
+
+        // build data
+        const newEntry = {
+            text: text,
+            hour: hour
+        }
+
+        // 1 get the old data
+
+        // 2. update the old data
+
+        // 3 strinigy and set the data
+        localStorage.setItem('saved', text);
+
+    });
+
+    // console.log(variable)
     //have to iterate through them and add an event listener
 }
 function addClass(hour){
